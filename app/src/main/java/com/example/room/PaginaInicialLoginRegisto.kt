4320d87a3514e2cfbb819ab0f547ef2ab4df2187
Joao Mirra <joaomirra@ipvc.pt>
2023-03-31 14:19:35 +0100

    Pagina Inicial Login e Registo

---
+++ app/src/main/java/com/example/room/PaginaInicialLoginRegisto.kt
@@ -0,0 +1,11 @@
+package com.example.room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaginaInicialLoginRegisto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_inicial_login_registo)
+    }
+}
