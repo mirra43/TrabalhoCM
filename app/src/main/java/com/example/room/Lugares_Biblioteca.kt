73f87bda02c6f9d09f2d894133acc6816dc27537
gpalmeida0 <100305068+gpalmeida0@users.noreply.github.com>
2023-04-02 16:55:54 +0100

    Design em desenvolvimento

---
+++ app/src/main/java/com/example/room/Lugares_Biblioteca.kt
@@ -0,0 +1,11 @@
+package com.example.room
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Lugares_Biblioteca : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lugares_biblioteca)
+    }
+}
