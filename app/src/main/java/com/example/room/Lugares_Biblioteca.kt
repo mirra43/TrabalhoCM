99dd28ee472d25fbe12d2c99c4aad06dabbf9af7
Joao Mirra <joaomirra@ipvc.pt>
2023-04-18 22:01:13 +0100

    Correcao de bugs

--- app/src/main/java/com/example/room/Lugares_Biblioteca.kt
+++ app/src/main/java/com/example/room/Lugares_Biblioteca.kt
@@ -38,6 +38,37 @@ class Lugares_Biblioteca : AppCompatActivity() {
         openMenuButton.setOnClickListener {
             drawerLayout.openDrawer(navDrawer)
         }
+
+        val request = ServiceBuilder.buildService(EndPoints::class.java)
+        val call = request.getBiblioteca()
+        call.enqueue(object : Callback<List<LugaresBiblioteca>> {
+            override fun onResponse(call: Call<List<LugaresBiblioteca>>, response: Response<List<LugaresBiblioteca>>) {
+                Log.d("ListarLugares", "onResponse: called")
+                if (response.isSuccessful){
+                    val lugares: List<LugaresBiblioteca>? = response.body() // get the data from the API
+                    for (lugar in lugares!!) { // loop over the items in the list
+                        val viewId = "l${lugar.id}" // get the ID of the corresponding view
+                        val view = findViewById<View>(resources.getIdentifier(viewId, "id", packageName)) // get a reference to the view
+                        val drawable = view.background as GradientDrawable // get a reference to the drawable object
+                        if (lugar.ocupada == 1) { // check if the "ocupada" field is 1
+                            val verdeColor = ContextCompat.getColor(this@Lugares_Biblioteca, R.color.verde) // get the color from colors.xml
+                            drawable.setColorFilter(verdeColor, PorterDuff.Mode.SRC_ATOP) // set the color filter to the "verde" color
+                        } else {
+                            val vermelhoColor = ContextCompat.getColor(this@Lugares_Biblioteca, R.color.vermelho) // get the color from colors.xml
+                            drawable.setColorFilter(vermelhoColor, PorterDuff.Mode.SRC_ATOP) // set the color filter to the "vermelho" color
+                        }
+                    }
+
+
+                } else {
+                    Log.d("Listar Lugares", "Response unsuccessful: ${response.code()}")
+                }
+            }
+            override fun onFailure(call: Call<List<LugaresBiblioteca>>, t: Throwable) {
+                Log.d("Listar Lugares", "onFailure: ${t.message}")
+                Toast.makeText(this@Lugares_Biblioteca, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     fun abrePaginaInicial(view: View) {
@@ -74,34 +105,4 @@ class Lugares_Biblioteca : AppCompatActivity() {
         //remover o toast
         Toast.makeText(applicationContext, "Dá Logout e Abre Página Login", Toast.LENGTH_SHORT).show()
     }
-        val request = ServiceBuilder.buildService(EndPoints::class.java)
-        val call = request.getBiblioteca()
-        call.enqueue(object : Callback<List<LugaresBiblioteca>> {
-            override fun onResponse(call: Call<List<LugaresBiblioteca>>, response: Response<List<LugaresBiblioteca>>) {
-                Log.d("ListarLugares", "onResponse: called")
-                if (response.isSuccessful){
-                    val lugares: List<LugaresBiblioteca>? = response.body() // get the data from the API
-                    for (lugar in lugares!!) { // loop over the items in the list
-                        val viewId = "l${lugar.id}" // get the ID of the corresponding view
-                        val view = findViewById<View>(resources.getIdentifier(viewId, "id", packageName)) // get a reference to the view
-                        val drawable = view.background as GradientDrawable // get a reference to the drawable object
-                        if (lugar.ocupada == 1) { // check if the "ocupada" field is 1
-                            val verdeColor = ContextCompat.getColor(this@Lugares_Biblioteca, R.color.verde) // get the color from colors.xml
-                            drawable.setColorFilter(verdeColor, PorterDuff.Mode.SRC_ATOP) // set the color filter to the "verde" color
-                        } else {
-                            val vermelhoColor = ContextCompat.getColor(this@Lugares_Biblioteca, R.color.vermelho) // get the color from colors.xml
-                            drawable.setColorFilter(vermelhoColor, PorterDuff.Mode.SRC_ATOP) // set the color filter to the "vermelho" color
-                        }
-                    }
-
-
-                } else {
-                    Log.d("Listar Lugares", "Response unsuccessful: ${response.code()}")
-                }
-            }
-            override fun onFailure(call: Call<List<LugaresBiblioteca>>, t: Throwable) {
-                Log.d("Listar Lugares", "onFailure: ${t.message}")
-                Toast.makeText(this@Lugares_Biblioteca, "${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
 }
